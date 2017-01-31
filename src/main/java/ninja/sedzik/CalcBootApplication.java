e80b78a5d636c21dd7471cfa5713ee9edf25d62c
AndrzejSędzik <paralotnik@interia.pl>
2017-01-31 23:24:44 +0100

    fuck it again

---
+++ src/main/java/ninja/sedzik/CalcBootApplication.java
@@ -0,0 +1,14 @@
+package ninja.sedzik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcBootApplication.class, args);
+	}
+
+
+}
